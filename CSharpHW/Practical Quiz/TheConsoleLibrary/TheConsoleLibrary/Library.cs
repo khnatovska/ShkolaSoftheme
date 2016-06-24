945dbb64cee13e6316117d5a8287869ca8d38efb
kgnatovska <k.hnatovska@gmail.com>
2016-06-25 00:23:47 +0300

    Practical quiz - TheConsoleLibrary (unclean)
    (..and full of bugs)

---
+++ CSharpHW/Practical Quiz/TheConsoleLibrary/TheConsoleLibrary/Library.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheConsoleLibrary
+{
+    public class Library
+    {
+        public List<Book> Shelf = new List<Book>();
+        Dictionary<string, int> GenreMatrix = new Dictionary<string, int>();
+        Dictionary<string, Book> GenreSummary = new Dictionary<string, Book>();
+        Dictionary<Book, IUser> UserJournal = new Dictionary<Book, IUser>();
+        Book Oldest;
+        Book Newest;
+        Book MostPopular;
+        
+
+        public Library()
+        {
+            CreateGenreMatrix();
+            CreateGenreSummary();
+            FillLibrary();
+        }
+
+        public void ReturnBook(Book book, IUser user)
+        {
+            UserJournal[book] = user;
+            UpdateLibrary(book);
+            UpdateGenreSummary(book);
+            Console.WriteLine("Returned.");
+        }
+
+        public void AddBook(Book book)
+        {
+            Shelf.Add(book);
+            UpdateGenreMatrix(book);
+            UpdateGenreSummary(book);
+            UpdateLibrary(book);
+        }
+
+        void UpdateLibrary(Book book)
+        {
+            if (Oldest == null || Oldest.PublicationYear > book.PublicationYear)
+            {
+                Oldest = book;
+            }
+            if (Newest == null || Newest.PublicationYear < book.PublicationYear)
+            {
+                Newest = book;
+            }
+            if (MostPopular == null || MostPopular.PopularityIndex <= book.PopularityIndex)
+            {
+                MostPopular = book;
+            }
+        }
+
+        public void ShowBooks(Genre genre)
+        {
+            foreach (Book book in Shelf)
+            {
+                if (book.Genre == genre)
+                {
+                    Console.WriteLine(book.GetBookInfo());
+                }
+            }
+        }
+
+        public void ShowBooks()
+        {
+            foreach (Book book in Shelf)
+            {
+                Console.WriteLine(book.GetBookInfo());
+            }
+        }
+
+        public string Summary()
+        {
+            var summary = "Library Summary\n\nBooks in genre:\n";
+            foreach(KeyValuePair<string, int> keyValue in GenreMatrix)
+            {
+                summary += keyValue.Key + ": " + keyValue.Value + " books\n";
+            }
+            summary += "\nThe oldest book: " + Oldest.GetBookInfo() + "\nThe newest book: " + Newest.GetBookInfo() + "\nMost popular book: " + MostPopular.GetBookInfo();
+            return summary;
+        }
+
+        void CreateGenreMatrix()
+        {
+            foreach (string genre in Enum.GetNames(typeof(Genre)))
+            {
+                GenreMatrix.Add(genre, 0);
+            }
+        }
+
+        void UpdateGenreMatrix(Book book)
+        {
+            GenreMatrix[book.Genre.ToString()] += 1;
+        }
+
+        void CreateGenreSummary()
+        {
+            foreach (string genre in Enum.GetNames(typeof(Genre)))
+            {
+                GenreSummary.Add(genre, null);
+            }
+        }
+
+        void UpdateGenreSummary(Book book)
+        {
+            if (GenreSummary[book.Genre.ToString()] == null || GenreSummary[book.Genre.ToString()].PopularityIndex <= book.PopularityIndex)
+            {
+                GenreSummary[book.Genre.ToString()] = book;
+            }
+        }
+
+        public string ShowGenreSummary(Genre genre)
+        {
+            var genreSummary = "Most popular book in " + genre.ToString() + ":\n";
+            if (GenreSummary[genre.ToString()] != null)
+            {
+                return genreSummary + GenreSummary[genre.ToString()].GetBookInfo();
+            }
+            return genreSummary + "not yet available";
+        }
+
+        void FillLibrary()
+        {
+            var bookList = new List<Book>()
+            {
+                new Book("Sapiens. A brief history of humankind", "Yuval Noah Harari", Genre.Nonfiction, 2014, 450, 2),
+                new Book("Moab is My Washpot", "Stephen Fry", Genre.Nonfiction, 1998, 387, 3),
+                new Book("C# in Depth", "Jon Skeet", Genre.Nonfiction, 2013, 245, 1),
+                new Book("La promesse de l'aube", "Romain Gary", Genre.Novel, 2001, 305, 7),
+                new Book("Paul Bowles", "Let in come down", Genre.Novel, 2008, 223, 4),
+                new Book("Scott Piligrim", "Bryan O'Malley", Genre.GraphicNovel, 2002, 64, 2),
+                new Book("Deadpool", "Fabian Nicieza", Genre.GraphicNovel, 1992, 38, 8),
+                new Book("Twenty Love Poems and a Song of Despair", "Pablo Neruda", Genre.Poetry, 2001, 180, 2),
+                new Book("Leaves of Grass", "Walt Whitman", Genre.Poetry, 2008, 201, 1),
+                new Book("The Sonnets", "William Shakespeare", Genre.Poetry, 2003, 12),
+                new Book("The Divine Comedy", "Dante Alighieri ", Genre.Poetry, 2000, 730, 8),
+                new Book("The Complete Sherlock Holmes", "Arthur Conan Doyle", Genre.Crime, 1969, 567, 11),
+                new Book("The Name of the Rose", "Umberto Eco", Genre.Crime, 2003, 631, 9),
+                new Book("Misery", "Stephen King", Genre.Crime, 2006, 268, 4),
+                new Book("The Woman in White", "Wilkie Collins", Genre.Crime, 1988, 189, 3),
+                new Book("A Streetcar Named Desire", "Tennessee Williams ", Genre.Drama, 2001, 98, 2),
+                new Book("Who's Afraid of Virginia Woolf? ", "Edward Albee", Genre.Drama, 1998, 78, 1),
+                new Book("Dracula", "Bram Stoker", Genre.Horror, 2002, 370, 7),
+                new Book("Frankenstein", "Mary Shelley", Genre.Horror, 1991, 248, 6),
+                new Book("The Shining", "Stephen King", Genre.Horror, 2001, 340, 9),
+                new Book("The Street of Crocodiles", "Bruno Schulz", Genre.ShortStory, 2005, 101, 3),
+                new Book("I Robot", "Isaac Asimov", Genre.ShortStory, 1989, 150, 2),
+                new Book("The Metamorphosis", "Franz Kafka", Genre.Novel, 2014, 354, 14),
+                new Book("The Curious Case of Benjamin Button", "F. Scott Fitzgerald", Genre.ShortStory, 2000, 134, 5),
+                new Book("Signs and Symbols", "Vladimir Nabokov", Genre.ShortStory, 2007, 211, 4),
+                new Book("A Perfect Day for Bananafish", "J. D. Salinger", Genre.ShortStory, 2003, 123, 2),
+                new Book("The Martian Chronicles", "Ray Bradbury", Genre.ScienceFiction, 1999, 341, 11),
+                new Book("The Martian", "Andy Weir", Genre.ScienceFiction, 2014, 279, 5),
+                new Book("Dune", "Frank Herbert", Genre.ScienceFiction, 2004, 356, 11),
+                new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", Genre.ScienceFiction, 2013, 354, 7),
+                new Book("1984", "George Orwell", Genre.ScienceFiction, 2015, 440, 15),
+                new Book("Ulysses", "James Joyce", Genre.Novel, 2004, 1015, 6),
+                new Book("One Hundred Years of Solitude", "Gabriel Garcia Marquez", Genre.Novel, 2016, 860, 10),
+                new Book("Alice's Adventures in Wonderland", "Lewis Carroll", Genre.Novel, 2010, 189, 9),
+                new Book("The Catcher in the Rye", "J. D. Salinger", Genre.Novel, 2009, 382, 5),
+                new Book("To the Lighthouse", "Virginia Woolf", Genre.Novel, 2009, 256, 3),
+                new Book("For Whom the Bell Tolls", "Ernest Hemingway", Genre.Novel, 2003, 234, 6),
+                new Book("Slaughterhouse-Five", "Kurt Vonnegut", Genre.Novel, 2015, 397, 7),
+                new Book("Twenty Thousand Leagues Under the Sea", "Jules Verne", Genre.Novel, 2008, 356, 9)
+            };
+
+            foreach (Book book in bookList)
+            {
+                AddBook(book);
+            }
+        }
+    }
+}
---
+++ CSharpHW/Practical Quiz/TheConsoleLibrary/TheConsoleLibrary/Program.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheConsoleLibrary
+{
+    class Program
+    {
+        static Library library = new Library();
+        static Validator validator = new Validator();
+        static IUser CurrentUser { get; set; }
+        
+        static void Main(string[] args)
+        {
+       
+            CurrentUser = AuthenticateUser();
+
+            Console.WriteLine(CurrentUser.GetFullInfo());
+
+            while (true)
+            {
+                GetCommand();
+            }
+
+            Console.ReadLine();
+        }
+
+        static IUser AuthenticateUser()
+        {
+            Console.WriteLine("Do you want to log in? (yes/no)");
+            string loginInput = Console.ReadLine();
+            ExitWanted(loginInput);
+            if (loginInput == "yes")
+            {
+                string nameOrEmail;
+                string password;
+                bool nameGiven = true;
+                Console.WriteLine("Enter name or email");
+                string nameOrEmailInput = Console.ReadLine();
+                ExitWanted(nameOrEmailInput);
+                if (nameOrEmailInput.Contains("@"))
+                {
+                    nameGiven = false;
+                }
+                nameOrEmail = nameOrEmailInput;
+                Console.WriteLine("Enter password");
+                string passwordInput = Console.ReadLine();
+                ExitWanted(passwordInput);
+                password = passwordInput;
+                var user = new User(nameOrEmail, password, nameGiven);
+                if (validator.ValidateUser(user))
+                {
+                    return user;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect login or password.");
+                    return AuthenticateUser();
+                }
+            }
+            else if (loginInput == "no")
+            {
+                return new AnonimousUser();
+            }
+            else {
+                Console.WriteLine("Invalid input.");
+                return AuthenticateUser();
+            }
+        }
+
+        static void GetCommand()
+        {
+            var input = Console.ReadLine();
+            ExitWanted(input);
+            switch (input)
+            {
+                case "Summary":
+                    Console.WriteLine(library.Summary());
+                    break;
+                case "MostPopularBook":
+                    MostPopularWanted();
+                    break;
+                case "FindBook":
+                    FindBookWanted();
+                    break;
+                case "TakeBook":
+                    if (CurrentUser.IsAuthorized)
+                    {
+                        TakeBookWanted();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Only logged in users can take books.");
+                    }
+                    break;
+                case "ReturnBook":
+                    if (CurrentUser.IsAuthorized)
+                    {
+                        ReturnBookWanted();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Only logged in users can return books.");
+                    }
+                    break;
+                case "AddBook":
+                    if (CurrentUser.IsAuthorized)
+                    {
+                        AddBookWanted();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Only logged in users can add books.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Command not recognized.");
+                    GetCommand();
+                    break;
+            }
+        }
+
+        static void MostPopularWanted()
+        {
+            Console.WriteLine("Please select genre:\n1 - Drama, 2 - Crime, 3 - Novel, 4 - GraphicNovel, 5 - Nonfiction, 6 - ScienceFiction, 7 - ShortStory, 8 - Horror, 9 - Poetry");
+            var input = Console.ReadLine();
+            ExitWanted(input);
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.Drama));
+                    break;
+                case "2":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.Crime));
+                    break;
+                case "3":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.Novel));
+                    break;
+                case "4":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.GraphicNovel));
+                    break;
+                case "5":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.Nonfiction));
+                    break;
+                case "6":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.ScienceFiction));
+                    break;
+                case "7":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.ShortStory));
+                    break;
+                case "8":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.Horror));
+                    break;
+                case "9":
+                    Console.WriteLine(library.ShowGenreSummary(Genre.Poetry));
+                    break;
+                case "back":
+                    return;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    break;
+            }
+        }
+
+        static BookFinder FindBookWanted()
+        {
+            var finder = new BookFinder(library);
+            Console.WriteLine("Find your book. Enter book title or author, or both separated by semicolon (;)");
+            var input = Console.ReadLine();
+            ExitWanted(input);
+            finder.Find(input);
+            finder.ShowBooksFound();
+            return finder;
+        }
+
+        static void TakeBookWanted()
+        {
+            var finder = FindBookWanted();
+            if (finder.BooksFound.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                var index = ConvertStringToBookIndex(finder.BooksFound.Count);
+                CurrentUser.TakeBook(finder.BooksFound[index]);
+            }
+        }
+
+        static void ReturnBookWanted()
+        {
+            CurrentUser.ShowBooks();
+            var index = ConvertStringToBookIndex(CurrentUser.Books.Count);
+            var book = CurrentUser.Books[index];
+            library.ReturnBook(book, CurrentUser);
+            CurrentUser.ReturnBook(book);
+        }
+
+        static void AddBookWanted()
+        {
+            var newBook = ConstructBook();
+            library.AddBook(newBook);
+        }
+
+        static void ExitWanted(string input)
+        {
+            if (input == "exit")
+                Environment.Exit(0);
+        }
+
+        static Book ConstructBook()
+        {
+            Console.WriteLine("Enter book title");
+            var title = Console.ReadLine();
+            Console.WriteLine("Enter book author");
+            var author = Console.ReadLine();
+            var genre = ConvertStringToGenre();
+            var year = ConvertStringToYear();
+            var pages = ConvertStringToPages();
+            var book = new Book(title, author, genre, year, pages);
+            Console.WriteLine("New book: {0}", book.GetBookInfo());
+            return book;
+        }
+
+        static Genre ConvertStringToGenre()
+        {
+            Console.WriteLine("Please select genre:\n1 - Drama, 2 - Crime, 3 - Novel, 4 - GraphicNovel, 5 - Nonfiction, 6 - ScienceFiction, 7 - ShortStory, 8 - Horror, 9 - Poetry");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    return Genre.Drama;
+                case "2":
+                    return Genre.Crime;
+                case "3":
+                    return Genre.Novel;
+                case "4":
+                    return Genre.GraphicNovel;
+                case "5":
+                    return Genre.Nonfiction;
+                case "6":
+                    return Genre.ScienceFiction;
+                case "7":
+                    return Genre.ShortStory;
+                case "8":
+                    return Genre.Horror;
+                case "9":
+                    return Genre.Poetry;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    return ConvertStringToGenre();
+            }
+        }
+
+        static int ConvertStringToYear()
+        {
+            Console.WriteLine("Enter publication year");
+            var input = Console.ReadLine();
+            int parsed;
+            if (int.TryParse(input, out parsed))
+            {
+                if (parsed <= DateTime.Today.Year && parsed > 0)
+                {
+                    return parsed;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid year.");
+                    return ConvertStringToYear();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                return ConvertStringToYear();
+            }
+        }
+
+        static int ConvertStringToPages()
+        {
+            Console.WriteLine("Enter number of pages");
+            var input = Console.ReadLine();
+            int parsed;
+            if (int.TryParse(input, out parsed))
+            {
+                if (parsed > 0)
+                {
+                    return parsed;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number of pages.");
+                    return ConvertStringToPages();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                return ConvertStringToPages();
+            }
+        }
+
+        static int ConvertStringToBookIndex(int maxIndex)
+        {
+            Console.WriteLine("Please select book from the list");
+            var input = Console.ReadLine();
+            int parsed;
+            if (int.TryParse(input, out parsed))
+            {
+                if (parsed <= maxIndex && parsed > 0)
+                {
+                    return parsed-1;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid book index.");
+                    return ConvertStringToPages();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                return ConvertStringToPages();
+            }
+        }
+    }
+}
