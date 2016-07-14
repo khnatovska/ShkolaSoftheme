e67f7ffd278b7ef095f1cf41d1b5f9bfcd65ae05
kgnatovska <k.hnatovska@gmail.com>
2016-07-15 00:01:20 +0300

    task 16 - EasyCollections
    EasyDictionary

---
+++ CSharpHW/16/EasyCollections/EasyCollections_EasyDictionary/EasyDictionary.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCollections_EasyDictionary
+{
+    public class EasyDictionary<TKey, TValue>
+    {
+        List<TKey> KeysList;
+        List<TValue> ValuesList;
+
+        public EasyDictionary()
+        {
+            KeysList = new List<TKey>();
+            ValuesList = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (KeysList.Contains(key))
+            {
+                throw new ArgumentException("Key is already present.");
+            }
+            KeysList.Add(key);
+            ValuesList.Add(value);
+        }
+
+        public void Remove(TKey key)
+        {
+            if (!KeysList.Contains(key))
+            {
+                throw new ArgumentException("Key is absent.");
+            }
+            var index = KeysList.IndexOf(key);
+            KeysList.RemoveAt(index);
+            ValuesList.RemoveAt(index);
+        }
+
+        public int Count()
+        {
+            return KeysList.Count;
+        }
+
+        public List<TKey> Keys()
+        {
+            return KeysList;
+        }
+
+        public List<TValue> Values()
+        {
+            return ValuesList;
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (KeysList.Contains(key))
+                return true;
+            else
+                return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            value = default(TValue);
+            if (KeysList.Contains(key))
+            {
+                value = ValuesList[KeysList.IndexOf(key)];
+                return true;
+            }
+            return false;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                TValue value;
+                try
+                {
+                    value = ValuesList[KeysList.IndexOf(key)];
+                    return value;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    throw new KeyNotFoundException("Key is absent.");
+                }
+            }
+            set
+            {
+                if (KeysList.Contains(key))
+                {
+                    throw new ArgumentException("Key is already present.");
+                }
+                KeysList.Add(key);
+                ValuesList.Add(value);
+            }
+        }
+
+        public void Print()
+        {
+            Console.Write("{");
+            for (var i = 0; i < KeysList.Count; i++)
+            {
+                Console.Write(" {0} : {1}, ", KeysList[i], ValuesList[i]);
+            }
+            Console.WriteLine(" }");
+        }
+    }
+}
---
+++ CSharpHW/16/EasyCollections/EasyCollections_EasyDictionary/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCollections_EasyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new EasyDictionary<string, string>();
+            d.Add("salinger", "rye");
+            d.Add("allen", "wheat");
+            d.Add("reno", "vassabi");
+            d.Add("bradbury", "dandelion");
+            d.Print();
+
+            try
+            {
+                d.Add("bradbury", "fahrenheit");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Exception caught. {0}", ex.Message);
+            }
+
+            try
+            {
+                d.Remove("freud");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Exception caught. {0}", ex.Message);
+            }
+
+            d.Remove("reno");
+            d.Print();
+
+            Console.WriteLine("For key = \"allen\", value = {0}.", d["allen"]);
+
+            d["saint-exupery"] = "baobab";
+            Console.WriteLine("For key = \"saint-exupery\", value = {0}.", d["saint-exupery"]);
+            
+            try
+            {
+                Console.WriteLine("For key = \"clover\", value = {0}.", d["clover"]);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string value = "";
+            if (d.TryGetValue("clover", out value))
+            {
+                Console.WriteLine("For key = \"clover\", value = {0}.", value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"clover\" is not found.");
+            }
+
+            if (d.TryGetValue("allen", out value))
+            {
+                Console.WriteLine("For key = \"allen\", value = {0}.", value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"allen\" is not found.");
+            }
+
+            if (!d.ContainsKey("irish"))
+            {
+                d.Add("irish", "clover");
+                Console.WriteLine("Value added for key = \"irish\": {0}", d["irish"]);
+            }
+            d.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
